36ecf4b60a212b0c06bd99424a5fc78bdab0d150
byron <47872200+byeoon@users.noreply.github.com>
2024-02-08 12:12:01 -0500

    Added extra modules, added authentication login

--- blinkcontrols.py
+++ blinkcontrols.py
@@ -1,14 +1,27 @@
 from blinkpy.blinkpy import Blink
-from tkinter import *
+from blinkpy.auth import Auth
 from blinkpy.api import *
+from tkinter import *
 from BCconfig import * # aware of the bad code practice, will fix later.
 import tkinter
+import requests
 
-# Change 'NAME OF SYNC DEVICE' and 'NAME OF CAMERA' with your camera and sync devices, respectively. Learn more in the README.
 blink = Blink()
-blink.start()
+blink.auth = Auth()
 screen = tkinter.Tk()
 
+headers = {
+    'Content-Type': 'application/json',
+}
+json_data = {
+    'unique_id': '00000000-0000-0000-0000-000000000000',
+    'password': BCconfig.password,
+    'email': BCconfig.email,
+}
+response = requests.post('https://rest-prod.immedia-semi.com/api/v5/account/login', headers=headers, json=json_data)
+print(response)
+
+blink.start()
 
 # When initialized, print some useful information.
 for name, camera in blink.cameras.items():
@@ -21,24 +34,38 @@ for name, camera in blink.cameras.items():
 # Arms the camera by enabling motion alerts.
 # Note: You can disable motion controls for individual cameras.
 def armCamera():
-    blink.sync["NAME OF SYNC DEVICE"].arm = True
+    blink.sync[BCconfig.syncmodule].arm = True
     blink.refresh()
 
 # Disarms the camera by disabling motion alerts.
 # Note: You can disable motion controls for individual cameras.
 def disarmCamera():
-    blink.sync["NAME OF SYNC DEVICE"].arm = False
+    blink.sync[BCconfig.syncmodule].arm = False
     blink.refresh()
     
     
 # Downloads all videos from the past week days of this app being created.
 # You can customize the delay, and since parameters.
 def downloadAllVideos():
-    # blink.download_videos('/home/blink', since='2023/01/01 12:00', delay=4)
+    # blink.download_videos('BCconfig.downloaddirectory', since='BCconfig.downloaddate', delay=BCconfig.downloaddelay)
     messagebox.showinfo("Videos have been downloaded successfully.")
+
+
+# Updates the picture that shows up when you open the app.
+def takePicture():
+    camera = blink.cameras[BCconfig.cameramodule]
+    camera.snap_picture()
+    blink.refresh(force=True)
+
+# uhh i forgot?
+def downloadLatest():
+    camera = blink.cameras[BCconfig.cameramodule]
+    camera.get_thumbnail()
+    camera.image_from_cache.raw
+
 
 # Initalize and pack UI elements here.
 top.title("Blink Controls")
 
-screen.configure(bg='white') 
+screen.configure(bg=BCconfig.backgroundcolor) 
 screen.mainloop()
