7bbd23b5c541e8fa212cefdd822656d5e1a21095
byron <47872200+byeoon@users.noreply.github.com>
2024-02-08 11:56:16 -0500

    Create BCconfig.py

---
+++ BCconfig.py
@@ -0,0 +1,13 @@
+# This is the configuration file for blinkcontrols. 
+
+# -- General Configuration -- 
+email = "CHANGE ME"
+password = "CHANGE ME"
+
+syncmodule = "CHANGE ME"
+cameramodule = "CHANGE ME"
+
+# -- Modules --
+
+# -- Customize --
+backgroundColor = "white" # Default: white
