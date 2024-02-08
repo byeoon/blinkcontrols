908ef7876838c382c1b3777123377f1706e695b6
byron <47872200+byeoon@users.noreply.github.com>
2024-02-08 12:12:12 -0500

    Added more config modules

--- BCconfig.py
+++ BCconfig.py
@@ -9,5 +9,10 @@ cameramodule = "CHANGE ME"
 
 # -- Modules --
 
+# Video Downloads
+downloaddirectory = "C:\"
+downloaddelay = 4 # Default: 4
+downloaddate = "2023/01/01 12:00" # Default 2024/02/01 12:00
+
 # -- Customize --
 backgroundColor = "white" # Default: white
