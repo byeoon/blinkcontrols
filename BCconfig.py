a4773eb07ca40809c9a8fb9104dfd64a5a97da0f
byron <47872200+byeoon@users.noreply.github.com>
2024-02-09 08:21:58 -0500

    fix variable name

--- BCconfig.py
+++ BCconfig.py
@@ -1,7 +1,7 @@
 # This is the configuration file for blinkcontrols. 
 
 # -- General Configuration -- 
-email = "CHANGE ME" # May also be called username in the blink API?
+email = "CHANGE ME"
 password = "CHANGE ME"
 
 syncmodule = "CHANGE ME"
@@ -15,4 +15,4 @@ downloaddelay = 4 # Default: 4
 downloaddate = "2023/01/01 12:00" # Default 2024/02/01 12:00
 
 # -- Customize --
-backgroundColor = "white" # Default: white
+backgroundcolor = "white" # Default: white
